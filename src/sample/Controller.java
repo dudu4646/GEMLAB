7c4cc206497056bf64a8de112b7c033934b31dd8
Dudu Vizel <wizelt@gmail.com>
2018-07-25 13:00:28 +0300

    OK button fixed

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
@@ -13,11 +14,19 @@ public class Controller {
     private TextField clientText;
     @FXML
     private ListView<Stone> stonesList;
+    @FXML
+    private Button okButton;
 
 
     @FXML
     public void insertClient(ActionEvent e){
         String s=((ToggleButton)e.getSource()).getText();
         clientText.setText(s);
+        checkCode();
+    }
+
+    @FXML
+    public void checkCode(){
+        okButton.setDisable((clientText.getText().length()>5)?false:true);
     }
 }
