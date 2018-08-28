df178307c6ef66503a4e22289b5fb9d93ed6ee59
Dudu Vizel <wizelt@gmail.com>
2018-08-28 15:46:38 +0300

    need to change the struct of the Stone class and set the printing right in the center.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,13 +6,18 @@ import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import sample.classes.Stone;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Controller {
     //לטעון רשימת שמות
     String[] shapes={"R" , "CU" , "CCR" , "SQ" , "SQEM" , "EM" , "REC" , "TRI", "PR", "HT" , "OTH", " "};
 
+    private List<Stone> list;
+
     @FXML
-    private TextField clientText;
+    private TextField clientText,clientRef,perCarat,total;
     @FXML
     private ListView<Stone> stonesList;
     @FXML
@@ -28,6 +33,15 @@ public class Controller {
     @FXML
     public void initialize(){
         shape.getItems().addAll(shapes);
+        Stone s1=new Stone("dudu","R","DG",1000,1000,1,false,false);
+        Stone s2=new Stone("test","R","DG",2000,2000,1,false,false);
+
+        list = new ArrayList<>();
+        list.add(s1);
+        list.add(s2);
+
+        stonesList.getItems().setAll(list);
+        stonesList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
     }
 
 
@@ -39,8 +53,23 @@ public class Controller {
     }
 
     @FXML
+    public void clikList(){
+        Stone s = (Stone)stonesList.getSelectionModel().getSelectedItems();
+    }
+
+    @FXML
     public void checkCode(){
-        okButton.setDisable((clientText.getText().length()>5)?false:true);
+        okButton.setDisable(clientText.getText().length() <= 5);
+    }
+
+    @FXML
+    public void addStone(){
+        clientRef.setText("");
+        perCarat.setText("");
+        total.setText("");
+        shape.setValue(" ");
+
+
     }
 
 
--- src/sample/classes/Stone.java
+++ src/sample/classes/Stone.java
@@ -1,4 +1,25 @@
 package sample.classes;
 
 public class Stone {
+    private String clientRef,shape,service;
+    private double per,total,weight;
+    private boolean seconderyService, addService;
+
+    public Stone(String clientRef, String shape, String service, double per, double total, double weight, boolean seconderyService, boolean addService) {
+        this.clientRef = clientRef;
+        this.shape = shape;
+        this.service = service;
+        this.per = per;
+        this.total = total;
+        this.weight = weight;
+        this.seconderyService = seconderyService;
+        this.addService = addService;
+    }
+
+    
+
+    @Override
+    public String toString() {
+    return "Ref: "+clientRef+", Shape: "+shape+" , Weight: "+weight+" , "+service;
+    }
 }
